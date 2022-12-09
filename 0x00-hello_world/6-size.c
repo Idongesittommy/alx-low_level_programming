b114258d33ba0815c1ea78080bcd1ece2306baa3
Idongesittommy <idongesitjohny78@gmail.com>
2022-12-08 17:53:19 -0800

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return:Always 0 (sucess)
+ */
+int main(void)
+
+{
+
+printf("The size of an int is: %lu.\n", (unsigned long)sizeof(i));
+printf("The size of an char is: %lu.\n", (unsigned long)sizeof(c));
+printf("The size of an float is: %lu.\n", (unsigned long)sizeof(f));
+printf("The size of an long init is: %lu.\n", (unsigned long)sizeof(lu));
+printf("The size of an long long init is: %lu.\n", (unsigned long)sizeof(llu));
+
+return (0);
+
+}
