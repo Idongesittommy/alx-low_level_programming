473adbe4071a6cfc4f8ed97ab4cd61d7fe8ba649
Idongesittommy <idongesitjohny78@gmail.com>
2022-12-14 12:40:50 -0800

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alphabet 10 times followed by new line
+ */
+void print_alphabet_x10(void)
+{
+	char la;
+	int ten;
+		for (ten = 0; ten <= 9; ten++)
+	{
+		for (la = 'a'; la <= 'z'; la++)
+			_putchar(la)
+	}
+	_putchar ('\n')
+}
