39a51c031189315703e0e3d5f038166fdce909c7
Idongesittommy <idongesitjohny78@gmail.com>
2022-12-14 08:20:48 -0800

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - print -putchar,followed by a newline
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	-putchar('-');
+	-putchar('p');
+	-putchar('u');
+	-putchar('t');
+	-putchar('c');
+	-putchar('h');
+	-putchar('a');
+	-putchar('r');
+	return (0);
+}
