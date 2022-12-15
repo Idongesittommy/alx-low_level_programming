597cbe4182158d55912d6bdd89054fba007f377e
Idongesittommy <idongesitjohny78@gmail.com>
2022-12-15 15:33:06 -0800

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,17 +12,24 @@ int print_sign(int n)
 if (n > 0)
 {
 
-_putchar(+);
+_putchar('+');
 return (1);
 
 }
-else
+else if (n == 0)
+{
 
-	_putchar(-);
-	return (-1);
+	_putchar(48);
+	return (0);
+}
+else if (n < 0)
+{
 
-	_putchar(n == 0);
-		return (0);
+	_putchar('-');
+}
+
+return (-1);
+
 
 }
 
