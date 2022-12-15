40d8c8f0545059aeac345557529f5c5068320370
Idongesittommy <idongesitjohny78@gmail.com>
2022-12-14 23:29:03 -0800

    is alphabets

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,7 +4,7 @@
  * @c: characters to be checked
  * Return: 1 if character is letter, 0 if not
  */
-int _isalpha(int c);
+int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
