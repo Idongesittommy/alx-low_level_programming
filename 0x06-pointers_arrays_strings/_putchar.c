4eb443c9014f00cfc1774235aa9eaf253af7ad89
Idongesittommy <idongesitjohny78@gmail.com>
2022-12-21 12:16:26 -0800

    strcat

---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *  * _putchar - prints the characters c to stdout
+ *   * @c: char to be printed
+ *    * Return: 1 0n success
+ *     */
+int _putchar(char c)
+{
+	        return (write(1, &c, 1));
+}
